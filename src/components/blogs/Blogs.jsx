8b5341bcd281049bea5fa644b168c81109ec13dc
rukonbd1 <rukonbd.dev@gmail.com>
2023-04-24 14:37:57 +0600

    server deploy

--- src/components/blogs/Blogs.jsx
+++ src/components/blogs/Blogs.jsx
@@ -7,7 +7,7 @@ const Blogs = () => {
     useEffect(()=>{
         const fetchBlogs=async()=>{
             try{
-                const response=await fetch('http://localhost:5000/blogs');
+                const response=await fetch('https://mongobd-server-rukonbdju.vercel.app/blogs');
                 const result= await response.json();
                 setBlogs(result);
             }
--- src/components/createPost/CreatePost.jsx
+++ src/components/createPost/CreatePost.jsx
@@ -7,7 +7,7 @@ const CreatePost = () => {
 
   const postData = async (data) => {
     try {
-      const response = await fetch("http://localhost:5000/blogs", {
+      const response = await fetch("https://mongobd-server-rukonbdju.vercel.app/blogs", {
         method: 'POST',
         headers: {
           "Content-Type": "application/json",
